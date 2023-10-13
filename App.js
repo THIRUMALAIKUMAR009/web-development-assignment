72045bdf2c25abf0101d02870de091058075f980
THIRUMALAIKUMAR.C <113839904+THIRUMALAIKUMAR009@users.noreply.github.com>
2023-10-13 14:25:02 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.js";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App.js">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
