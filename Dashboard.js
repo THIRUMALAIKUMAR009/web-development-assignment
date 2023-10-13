d695270aee8adf3af74c08e18c37a6fc493c814b
THIRUMALAIKUMAR.C <113839904+THIRUMALAIKUMAR009@users.noreply.github.com>
2023-10-13 14:24:07 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,110 @@
+import React from "package.json"
+import React from "react";
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div className="product">
+        <p>Product (2)</p>
+      </div>
+      <div className="customers">
+        <p>Customers</p>
+      </div>
+      <div className="income">
+        <p>Income</p>
+      </div>
+      <div className="promote">
+        <p>Promote</p>
+      </div>
+      <div className="help">
+        <p>Help</p>
+      </div>
+    </div>
+  );
+}
+function DashBoard() {
+  return (
+    <div>
+      <h1>Hello Shahrukh</h1>
+      <h2>Open with</h2>
+      <div className="product-sell">
+        <p>Product Sell</p>
+        <p>$89k +27% this week</p>
+      </div>
+      <div className="total-customers">
+        <p>65% Total New Customers</p>
+      </div>
+      <h2>Jon</h2>
+      <div className="qustom">
+        <p>Qustom that buy</p>
+      </div>
+      <div className="dec">
+        <p>Dec</p>
+        <p>$2.4k 42% this month</p>
+      </div>
+      <div className="nov">
+        <p>Nov</p>
+        <p>Quarterly</p>
+      </div>
+      <div className="oct">
+        <p>Customers</p>
+        <p>$2.4k +2% this month</p>
+      </div>
+      <div className="aug-sep">
+        <p>Aug</p>
+        <p>Sep</p>
+        <p>37.8% this month</p>
+      </div>
+      <h2>Overview</h2>
+      <div className="apr-may">
+        <p>Apr</p>
+        <p>May</p>
+        <p>$198k</p>
+      </div>
+    </div>
+  );
+}
+
+function SalesDashboard() {
+  return (
+    <div>
+      <h1>65% Total New Customers</h1>
+      <h2>Jan</h2>
+      <p>Customers shot they products</p>
+      <div className="customers-quarterly">
+        <h2>Customers</h2>
+        <p>Quarterly</p>
+      </div>
+      <h2>Monthly Coming</h2>
+      <h2>Overview</h2>
+      <div className="monthly-sales">
+        <h2>Dec</h2>
+        <h2>NOV</h2>
+        <h2>Oct</h2>
+        <h2>Sep</h2>
+        <h2>Aug</h2>
+        <h2>Jun</h2>
+        <h2>Feb</h2>
+      </div>
+      <div className="product-sell">
+        <p>Product Sell</p>
+        <p>$45.99</p>
+        <p>$45.99</p>
+        <p>32 in stock</p>
+        <p>70</p>
+        <p>32 in stock</p>
+      </div>
+      <h2>Sarphens Illustration</h2>
+      <div className="apt">
+        <h2>May</h2>
+        <p>Abstract 30</p>
+      </div>
+    </div>
+  );
+}
+export default Dashboard; App.js;
+
+
+
+
+
